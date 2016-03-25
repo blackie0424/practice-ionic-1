287433356f0d501a99f35fac8f11f1fa75e8803c
blackie0424 <blackie0424@gmail.com>
2016-03-25 18:35:03 +0800

    get random users data and display to prage2

--- www/js/app.js
+++ www/js/app.js
@@ -33,12 +33,29 @@ angular.module('starter', ['ionic'])
 
     .state('page2',{
         url:"/page2",
-        templateUrl:"templates/page2.html"
+        templateUrl:"templates/page2.html",
+        controller:"Page2Ctrl"
     });
 
 
 
     $urlRouterProvider.otherwise('/main');
+})
+
+.factory('userService', function ($http) {
+  return {
+      getUsers:function(){
+          return $http.get('https://randomuser.me/api/?results=10').then(function(response){
+              return response.data.results;
+          });
+      }
+  };
+})
+
+.controller('Page2Ctrl',function($scope,userService){
+    userService.getUsers().then(function(users){
+        $scope.users = users;
+    });
 })
 
 .controller('MainCtrl',function(){
