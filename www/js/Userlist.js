9234308af92511aed32cf525c2820d1d458cff9a
blackie0424 <blackie0424@gmail.com>
2016-03-28 14:20:02 +0800

    modify formate

--- www/js/Userlist.js
+++ www/js/Userlist.js
@@ -1,42 +1,42 @@
 angular.module('starter.UserList', ['ionic'])
 
-.config(function($stateProvider){
+.config(function($stateProvider) {
     $stateProvider
-    .state('UserList',{
-        url:"/UserList",
-        templateUrl:"templates/UserList.html",
-        controller:"UserListCtrl"
-    })
+        .state('UserList', {
+            url: "/UserList",
+            templateUrl: "templates/UserList.html",
+            controller: "UserListCtrl"
+        })
 
-    .state('UserListDetail',{
-    	url:"/UserListDetail/:index",
-    	templateUrl:"templates/UserListDetail.html",
-    	controller:"UserListDetailCtrl"
+    .state('UserListDetail', {
+        url: "/UserListDetail/:index",
+        templateUrl: "templates/UserListDetail.html",
+        controller: "UserListDetailCtrl"
     });
 })
 
-.factory('userService', function ($http) {
-	var users = [];
-  	return {
-    	getUsers:function(){
-        	return $http.get('https://randomuser.me/api/?results=10').then(function(response){
-            	users = response.data.results;
-            	return users;
-          	});
-      	},
-      	getUser:function(index){
-      		return users[index];
-      	}
-  	};
+.factory('userService', function($http) {
+    var users = [];
+    return {
+        getUsers: function() {
+            return $http.get('https://randomuser.me/api/?results=10').then(function(response) {
+                users = response.data.results;
+                return users;
+            });
+        },
+        getUser: function(index) {
+            return users[index];
+        }
+    };
 })
 
-.controller('UserListCtrl',function($scope,userService){
-    userService.getUsers().then(function(users){
+.controller('UserListCtrl', function($scope, userService) {
+    userService.getUsers().then(function(users) {
         $scope.users = users;
     });
 })
 
-.controller('UserListDetailCtrl',function($scope,$stateParams,userService){
-	var index = $stateParams.index;
-	$scope.item = userService.getUser(index);
+.controller('UserListDetailCtrl', function($scope, $stateParams, userService) {
+    var index = $stateParams.index;
+    $scope.item = userService.getUser(index);
 });
