8730d37b3e125a6d2744467f80bb6721e0d1647c
blackie0424 <blackie0424@gmail.com>
2016-03-28 14:18:05 +0800

    display userlistdetail when user click the user item

--- www/js/Userlist.js
+++ www/js/Userlist.js
@@ -6,21 +6,37 @@ angular.module('starter.UserList', ['ionic'])
         url:"/UserList",
         templateUrl:"templates/UserList.html",
         controller:"UserListCtrl"
+    })
+
+    .state('UserListDetail',{
+    	url:"/UserListDetail/:index",
+    	templateUrl:"templates/UserListDetail.html",
+    	controller:"UserListDetailCtrl"
     });
 })
 
 .factory('userService', function ($http) {
-  return {
-      getUsers:function(){
-          return $http.get('https://randomuser.me/api/?results=10').then(function(response){
-              return response.data.results;
-          });
-      }
-  };
+	var users = [];
+  	return {
+    	getUsers:function(){
+        	return $http.get('https://randomuser.me/api/?results=10').then(function(response){
+            	users = response.data.results;
+            	return users;
+          	});
+      	},
+      	getUser:function(index){
+      		return users[index];
+      	}
+  	};
 })
 
 .controller('UserListCtrl',function($scope,userService){
     userService.getUsers().then(function(users){
         $scope.users = users;
     });
+})
+
+.controller('UserListDetailCtrl',function($scope,$stateParams,userService){
+	var index = $stateParams.index;
+	$scope.item = userService.getUser(index);
 });
